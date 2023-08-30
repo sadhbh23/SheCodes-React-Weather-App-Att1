4c67458cb98fb0775171210b5a53d685c5736ff3
sadhbh23 <127418826+sadhbh23@users.noreply.github.com>
2023-08-30 18:13:41 +0100

    Updated App.js
    Removed preset JSX content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
+
+import SearchEngine from "./SearchEngine";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SearchEngine />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Hello, World!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
